4f1ad98afc4704a7c2aebc4345620430fa522035
mihailbaranov <problemaprod@gmail.com>
2024-02-24 14:50:10 +0000

    add contacts.html, change order_list.html, date_list.html

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .apps import OrderConfig
-from .views import OderListView, OrderCreateView, OrderDeleteView, main_page
+from .views import OderListView, OrderCreateView, OrderDeleteView, main_page, contact
 
 app_name = OrderConfig.name
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('list/', OderListView.as_view(), name='order_list'),
     path('create/<int:pk>/', OrderCreateView.as_view(), name='order_create'),
     path('delete/<int:pk>/', OrderDeleteView.as_view(), name='order_delete'),
+    path('contact/', contact, name='contact'),
 ]
--- order/views.py
+++ order/views.py
@@ -14,6 +14,12 @@ class OderListView(ListView):
     model = Order
     template_name = 'order/order_list.html'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_staff:
+            return queryset  # Если есть право доступа, то пользователь видит все рассылки
+        return queryset.filter(user=self.request.user)
+
 
 class OrderCreateView(CreateView):
     model = Order
@@ -41,11 +47,11 @@ class OrderDeleteView(DeleteView):
         elif self.object.user != self.request.user:
             raise Http404
 
-    def form_valid(self, form):
-        cancel_order(self.object.date, self.object.half_carcasses_quantity, self.object.by_product_quantity)
-        return super().form_valid(form)
 
 
 def main_page(request):
-
     return render(request, template_name='order/index.html')
+
+
+def contact(request):
+    return render(request, template_name='order/contacts.html')
